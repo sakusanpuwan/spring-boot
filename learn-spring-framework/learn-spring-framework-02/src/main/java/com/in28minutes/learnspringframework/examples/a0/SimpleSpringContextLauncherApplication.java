8350ce259814410dc2594e357f5769f231804c38
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-10-06 16:55:59 +0100

    added stream method to sout all initialised bean names

--- learn-spring-framework/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
+++ learn-spring-framework/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 
 @Configuration
 @ComponentScan
@@ -13,6 +15,7 @@ public class SimpleSpringContextLauncherApplication {
 
         var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class);
 
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
 
