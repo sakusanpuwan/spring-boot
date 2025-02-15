28a06034e47671ad5149a85c6cd0334e3324d8df
sakusanpuwan <sakusanpuwan98@gmail.com>
2025-02-15 19:35:07 +0000

    Add internationalisation to response via request header

--- learn-restful-api/src/main/java/com/rest/webservices/restful_web_services/hello/HelloWorldController.java
+++ learn-restful-api/src/main/java/com/rest/webservices/restful_web_services/hello/HelloWorldController.java
@@ -1,9 +1,21 @@
 package com.rest.webservices.restful_web_services.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @RequestMapping(method = RequestMethod.GET,path = "/hello-world-request")
     public String helloWorldRequest() {
@@ -23,5 +35,13 @@ public class HelloWorldController {
     @RequestMapping("/search")
     public String search(@RequestParam("keyword") String keyword) {
         return "Search results for: " + keyword;
+    }
+
+    // Use Accept-Language header in request to pass in language
+    // Accept-Language | de   -> picks up messages_de.properties
+    @GetMapping("/hello-world-internationalised")
+    public String helloWorldInternationalised() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message", locale);
     }
 }
